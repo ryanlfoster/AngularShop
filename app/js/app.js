41104197ed73999fa1461a07599c76f9e0d52544
Mike Godfrey <mjgodfrey83@gmail.com>
2013-09-17 21:19:30 +0100

    Basket service plus controllers and views for Basket and DeliveryMethod.

--- app/js/app.js
+++ app/js/app.js
@@ -3,19 +3,54 @@
 
 // Declare app level module which depends on filters, and services
 angular.module('cmcShop', ['ui.bootstrap', 'cmcShop.controllers'])
-   .config(function($provide) {
-      $provide.value('products', function() {
-         var products = [{
-               image: 'http://placekitten.com/200/200',
-               text: 'More Cats'
-            }, {
-               image: 'http://placekitten.com/300/200',
-               text: 'Extra Kittys'
-            }, {
-               image: 'http://placekitten.com/400/200',
-               text: 'Lots Of Felines'
-            }];
-         products.type = 'Kitten';
-         return products;
-      });
-   });
+   .factory('Products', function() {
+      var products = [{
+            id: 1,
+            image: 'http://placekitten.com/200/200',
+            text: 'More Cats',
+            price: 60
+         }, {
+            id: 2,
+            image: 'http://placekitten.com/300/200',
+            text: 'Extra Kittys',
+            price: 90
+         }, {
+            id: 3,
+            image: 'http://placekitten.com/400/200',
+            text: 'Lots Of Felines',
+            price: 75
+         }];
+      products.type = 'Kitten';
+      return products;
+   })
+   .factory('DeliveryMethods', function() {
+      return [{
+         type: 'Collect',
+         price: 0
+      }, {
+         type: 'Courier',
+         price: 25
+      }];
+   })
+   .factory('Basket', function($rootScope) {
+      var items = [];
+
+      var Basket = {
+         ITEM_ADDED: 'item_added',
+         add: function(item) {
+            items.push(item);
+            $rootScope.$broadcast(Basket.ITEM_ADDED);
+         },
+         getItems: function() {
+            return items;
+         }
+      };
+
+      return Basket;
+   })
+   .config(['$routeProvider', function($routeProvider) {
+      $routeProvider
+         .when('/basket', { templateUrl: 'partials/basket.html', controller: 'BasketCtrl' })
+         .when('/deliveryMethod', { templateUrl: 'partials/deliveryMethod.html', controller: 'DeliveryMethodCtrl' })
+         .otherwise({ redirectTo: '/basket' });
+   }]);
--- app/js/controllers.js
+++ app/js/controllers.js
@@ -3,8 +3,20 @@
 /* Controllers */
 
 angular.module('cmcShop.controllers', [])
-  .controller('ProductCarouselCtrl', ['$scope', 'products', function($scope, products) {
-     var slides = $scope.slides = products();
+  .controller('ProductCarouselCtrl', ['$scope', 'Products', 'Basket',  function($scope, Products, Basket) {
+     $scope.products = Products;
 
+     $scope.buy = function(product) {
+        Basket.add(product);
+     };
+  }])
+  .controller('BasketCtrl', ['$scope', 'Basket', function($scope, Basket) {
+     $scope.items = [];
 
+     $scope.$on(Basket.ITEM_ADDED, function() {
+        $scope.items = Basket.getItems();
+     });
+  }])
+  .controller('DeliveryMethodCtrl', ['$scope', 'DeliveryMethods', function($scope, DeliveryMethods) {
+     $scope.methods = DeliveryMethods;
   }]);
