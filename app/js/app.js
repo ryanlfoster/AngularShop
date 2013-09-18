09a0cc72e04911141e44c5ade589e002c36e54d0
Mike Godfrey <mjgodfrey83@gmail.com>
2013-09-18 07:57:17 +0100

    Tidy up to first pass.

--- app/js/app.js
+++ app/js/app.js
@@ -2,59 +2,7 @@
 
 
 // Declare app level module which depends on filters, and services
-angular.module('cmcShop', ['ui.bootstrap', 'cmcShop.controllers'])
-   .factory('Products', function() {
-      var products = [{
-            id: 1,
-            image: 'http://placekitten.com/200/200',
-            text: 'More Cats',
-            price: 60
-         }, {
-            id: 2,
-            image: 'http://placekitten.com/300/200',
-            text: 'Extra Kittys',
-            price: 90
-         }, {
-            id: 3,
-            image: 'http://placekitten.com/400/200',
-            text: 'Lots Of Felines',
-            price: 75
-         }];
-      products.type = 'Kitten';
-      return products;
-   })
-   .factory('DeliveryMethods', function() {
-      return [{
-         type: 'Collect',
-         price: 0
-      }, {
-         type: 'Courier',
-         price: 25
-      }];
-   })
-   .factory('Basket', function($rootScope) {
-      var items = [];
-      var deliveryMethod;
-
-      var Basket = {
-         ITEM_ADDED: 'item_added',
-         add: function(item) {
-            items.push(item);
-            $rootScope.$broadcast(Basket.ITEM_ADDED);
-         },
-         getItems: function() {
-            return items;
-         },
-         setDeliveryMethod: function(method) {
-            deliveryMethod = method;
-         },
-         getDeliveryMethod: function() {
-            return deliveryMethod;
-         }
-      };
-
-      return Basket;
-   })
+angular.module('cmcShop', ['ui.bootstrap', 'cmcShop.controllers', 'cmcShop.services'])
    .config(['$routeProvider', function($routeProvider) {
       $routeProvider
          .when('/basket', { templateUrl: 'partials/basket.html', controller: 'BasketCtrl' })
--- app/js/services.js
+++ app/js/services.js
@@ -1,9 +1,55 @@
 'use strict';
 
-/* Services */
+angular.module('cmcShop.services', [])
+   .factory('Products', function() {
+      var products = [{
+            id: 1,
+            image: 'http://placekitten.com/200/200',
+            text: 'More Cats',
+            price: 60
+         }, {
+            id: 2,
+            image: 'http://placekitten.com/300/200',
+            text: 'Extra Kittys',
+            price: 90
+         }, {
+            id: 3,
+            image: 'http://placekitten.com/400/200',
+            text: 'Lots Of Felines',
+            price: 75
+         }];
+      products.type = 'Kitten';
+      return products;
+   })
+   .factory('DeliveryMethods', function() {
+      return [{
+         type: 'Collect',
+         price: 0
+      }, {
+         type: 'Courier',
+         price: 25
+      }];
+   })
+   .factory('Basket', function($rootScope) {
+      var items = [];
+      var deliveryMethod;
 
+      var Basket = {
+         ITEM_ADDED: 'item_added',
+         add: function(item) {
+            items.push(item);
+            $rootScope.$broadcast(Basket.ITEM_ADDED);
+         },
+         getItems: function() {
+            return items;
+         },
+         setDeliveryMethod: function(method) {
+            deliveryMethod = method;
+         },
+         getDeliveryMethod: function() {
+            return deliveryMethod;
+         }
+      };
 
-// Demonstrate how to register services
-// In this case it is a simple value service.
-angular.module('myApp.services', []).
-  value('version', '0.1');
+      return Basket;
+   });
